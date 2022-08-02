0fad7ed3b28bf83d706373c55ce5f876823e5bef
Vladimir Kolchurin <18503099+kolchurinvv@users.noreply.github.com>
2022-08-02 20:23:13 +0300

    no swearing

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,40 +1,39 @@
 <script setup>
-import { ref } from 'vue'
+import { ref } from "vue"
 
 defineProps({
-  msg: String
+  msg: String,
 })
 
 const count = ref(0)
 </script>
 
 <template>
-<div class="border">
+  <div class="border">
+    <h1>{{ msg }}</h1>
+    <h2>Путин ХУЙЛО</h2>
+    <mark>материться плохо</mark>
 
-  <h1>{{ msg }}</h1>
-  <h2>Путин ХУЙЛО</h2>
-  <p>lol</p>
+    <div class="card">
+      <button type="button" @click="count++">count is {{ count }}</button>
+      <p>
+        Edit
+        <code>components/Bay Bay World.vue</code> to test HMR
+      </p>
+    </div>
 
-  <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
     <p>
-      Edit
-      <code>components/Bay Bay World.vue</code> to test HMR
+      Check out
+      <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
+        >create-vue</a
+      >, the official Vue + Vite starter
     </p>
-  </div>
-
-  <p>
-    Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >create-vue</a
-    >, the official Vue + Vite starter
-  </p>
-  <p>
-    Install
-    <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
-    in your IDE for a better DX
-  </p>
-  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
+    <p>
+      Install
+      <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
+      in your IDE for a better DX
+    </p>
+    <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
   </div>
 </template>
 
@@ -45,6 +44,6 @@ const count = ref(0)
 .border {
   border-radius: 5%;
   border: 5px solid aqua;
-  box-shadow:10px -0px 1px red;
+  box-shadow: 10px -0px 1px red;
 }
 </style>
