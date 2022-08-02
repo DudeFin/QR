bdb5f7fa52ae64d2d5a2b083c55bc1f5ade89243
yurii <suprun741@gmail.com>
2022-08-02 17:30:58 +0300

    first

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,49 @@
+<script setup>
+import { ref } from 'vue'
+
+defineProps({
+  msg: String
+})
+
+const count = ref(0)
+</script>
+
+<template>
+<div class="border">
+
+  <h1>{{ msg }}</h1>
+  <h2>Путин ХУЙЛО</h2>
+
+  <div class="card">
+    <button type="button" @click="count++">count is {{ count }}</button>
+    <p>
+      Edit
+      <code>components/Bay Bay World.vue</code> to test HMR
+    </p>
+  </div>
+
+  <p>
+    Check out
+    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
+      >create-vue</a
+    >, the official Vue + Vite starter
+  </p>
+  <p>
+    Install
+    <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
+    in your IDE for a better DX
+  </p>
+  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
+  </div>
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+.border {
+  border-radius: 5%;
+  border: 5px solid aqua;
+  box-shadow:10px -0px 1px red;
+}
+</style>
---
+++ src/components/anotherComponent.vue
@@ -0,0 +1,11 @@
+<script>
+</script>
+<template>
+<h1>helooooooooooo!</h1>
+<slot class="dotted"></slot>
+</template>
+<style scoped>
+.dotted {
+    color: red;
+}
+</style>
